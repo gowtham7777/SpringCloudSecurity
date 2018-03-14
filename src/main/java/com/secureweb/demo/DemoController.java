ea9f6b5d3d0f1be704bd18dde2bdaeba77bc096b
chiguripati, Gowtham Chiguripati <Gowtham.Chiguripati@t-mobile.com>
2018-03-13 20:50:03 -0400

    ignore login page

--- src/main/java/com/secureweb/demo/DemoController.java
+++ src/main/java/com/secureweb/demo/DemoController.java
@@ -3,14 +3,22 @@ package com.secureweb.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Controller
 @EnableOAuth2Sso
-public class DemoController {
+public class DemoController extends WebSecurityConfigurerAdapter{
 
     @RequestMapping("/")
     public String  homePage()
     {
         return "homePage";
+    }
+
+    @Override
+    protected  void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests().antMatchers("/","login**").permitAll()
+                .anyRequest().authenticated();
     }
 }
